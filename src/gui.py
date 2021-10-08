b46bd94b01c596c1e39eed78cdda322362cacd55
snowboardit <snowboardit@me.com>
2021-10-08 16:52:27 -0400

    API and GUI improvements

--- src/gui.py
+++ src/gui.py
@@ -5,7 +5,7 @@ UPDATE_FREQUENCY_MILLISECONDS = 5 * 1000
 
 layout = [
             [sg.Text("Welcome to TradingStats!")],
-            [sg.Text("Account Balance (Margin): "), sg.Text("Account Balance (Cash): ")],
+            [sg.Text("Account Balance (Margin): "), sg.Text("0", key="-MARGIN_BAL-"), sg.Text("Account Balance (Cash): "), sg.Text("0", key="-CASH_BAL-")],
             [sg.Text("{ Balance graph goes here }")]
           ]
 
@@ -15,9 +15,10 @@ window = sg.Window("TradingStats",
                    grab_anywhere=True,
                    margins=(15, 15))
 
-def update():
+def update(window):
   print("updated")
-  get_assets()
+  # get_assets()
+  window["-MARGIN_BAL-"].update(str(UPDATE_FREQUENCY_MILLISECONDS))
 
 # Create an event loop -- window.read(timeout=UPDATE_FREQUENCY_MILLISECONDS)
 while True:
@@ -27,6 +28,6 @@ while True:
     if event == sg.WIN_CLOSED:
         break
         
-    update()
+    update(window)
 
 window.close()
--- src/main.py
+++ src/main.py
@@ -1,25 +1,36 @@
 # Imports
 import core.query_prv_balance as qb
 import core.query_pub_assets as qpa
+import core.query_prv_open_orders as qoo
 
+## Setup
+CONFIG_PATH = 'config.json'
+
+# fetch all open orders - cash and margin
+# @click.option("--symbol", type=str, default=None)
+# @click.option("--account", type=click.Choice(['cash', 'margin']), default="cash", help="account category")
+# @click.option("--config", type=str, default="config.json", help="path to the config file")
+# @click.option("--verbose/--no-verbose", default=False)
+def get_openCashOrders():
+  return qoo.run(None, 'cash', CONFIG_PATH, True)
+
+def get_openMarginOrders():
+  return qoo.run(None, 'margin', CONFIG_PATH, True)
+
+# fetch margin and cash account balance for all assets
 # @click.option("--asset", type=str, default=None,
 #               help='optional, if none, return all assets with non-empty balance. You can specify an asset (e.g. "BTC")')
 # @click.option("--account", type=click.Choice(['cash', 'margin']), default="cash", help="cash (default) or margin")
 # @click.option("--show-all/--no-show-all", default=False, help="show all balances, including empty balances")
 # @click.option("--config", type=str, default="config.json", help="path to the config file")
 # @click.option("--verbose/--no-verbose", default=False)
-
-## Setup
-CONFIG_PATH = 'config.json'
-
-# fetch margin account balance for all assets & print to console
 def get_marginBal():
   return qb.run(None, 'margin', True, CONFIG_PATH, True)
 
-# fetch cash account balance for all assets & print to console
 def get_cashBal():
   return qb.run(None, 'cash', True, CONFIG_PATH, True)
 
 # fetch something public
+# @click.option("--config", type=str, default="config.json")
 def get_assets():
   return qpa.run(CONFIG_PATH)
