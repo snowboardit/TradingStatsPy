f162793e6ee3dc5092d6c2c752ecff25318c5be9
Max Lareau <max@maxlareau.com>
2021-10-08 09:03:08 -0400

    GUI additions

--- src/gui.py
+++ src/gui.py
@@ -6,13 +6,13 @@ UPDATE_FREQUENCY_MILLISECONDS = 20 * 1000
 layout = [
             [sg.Text("Welcome to TradingStats!")],
             [sg.Text("Account Balance (Margin): "), [], sg.Text("Account Balance (Cash): ", [])],
-            [sg.Text("{ Balance graph goes here }")],
-            [sg.Button("OK")]
+            [sg.Text("{ Balance graph goes here }")]
           ]
 
-window = sg.Window("TradingStats",
+window = sg.Window(
+                   "TradingStats",
                    layout,
-                   text_justification='c',
+                   justification='c',
                    no_titlebar=True,
                    grab_anywhere=True,
                    margins=(15, 15)
@@ -28,8 +28,9 @@ while True:
     event, values = window.read(timeout=UPDATE_FREQUENCY_MILLISECONDS)
     # End program if user closes window or
     # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
+    if event == sg.WIN_CLOSED:
         break
+    
     update()
 
 window.close()
